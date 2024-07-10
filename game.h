7fcf17b25da6863644ee57f988839933329e5158
JorVor <kongphop.kayoonvichien@gmail.com>
2024-07-10 21:47:39 +0700

    JJv.13

--- game.h
+++ game.h
@@ -46,4 +46,24 @@ int doYesNo()
     } while (ans!='y'||ans!='Y'||ans!='n'||ans!='N');
 }
 
+int prologue()
+{
+    system("clear");
+
+    cout<<"Before the noodle shop opened, our family knew the harsh reality of poverty and hunger. \n\n";
+    sleep(2);
+    cout<<"Once the shop began to flourish, it brought us financial stability and allowed my parents to help others in need. \n\n";
+    sleep(3);
+    cout<<"Inspired by their generosity, I now carry on the family business with a mission: \n\n";
+    sleep(2);
+    cout<<"to sustain our livelihood while generating enough profit to support those struggling with poverty and hunger. \n\n";
+    sleep(3);
+    cout<<"Welcome to our noodle shop, where every bowl served brings hope to our community. \n\n";
+    
+    pressEnterToContinue();
+    system("clear");
+
+    return 0;
+}
+
 #endif
